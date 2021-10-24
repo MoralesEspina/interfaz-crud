c99103a7e21d8f73bce9e3c4feefa14de42c7807
MoralesEspina <60724084+MoralesEspina@users.noreply.github.com>
2021-10-24 12:05:46 -0600

    Agregar Persona

--- src/app/tablas/tb-persona/tb-persona-datasource.ts
+++ src/app/tablas/tb-persona/tb-persona-datasource.ts
@@ -21,16 +21,8 @@ export class TbPersonaDataSource extends DataSource<TbPersonaItem>  {
     super();
   }
 
-
-  /**
-   * Connect this data source to the table. The table will only update when
-   * the returned stream emits new items.
-   * @returns A stream of the items to be rendered.
-   */
   connect(): Observable<TbPersonaItem[]> {
     if (this.paginator && this.sort) {
-      // Combine everything that affects the rendered data into one update
-      // stream for the data-table to consume.
       return merge(observableOf(this.data), this.paginator.page, this.sort.sortChange)
         .pipe(map(() => {
           return this.getPagedData(this.getSortedData([...this.data ]));
@@ -40,16 +32,8 @@ export class TbPersonaDataSource extends DataSource<TbPersonaItem>  {
     }
   }
 
-  /**
-   *  Called when the table is being destroyed. Use this function, to clean up
-   * any open connections or free any held resources that were set up during connect.
-   */
   disconnect(): void {}
 
-  /**
-   * Paginate the data (client-side). If you're using server-side pagination,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
   private getPagedData(data: TbPersonaItem[]): TbPersonaItem[] {
     if (this.paginator) {
       const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
@@ -59,10 +43,7 @@ export class TbPersonaDataSource extends DataSource<TbPersonaItem>  {
     }
   }
 
-  /**
-   * Sort the data (client-side). If you're using server-side sorting,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
+
   private getSortedData(data: TbPersonaItem[]): TbPersonaItem[] {
     if (!this.sort || !this.sort.active || this.sort.direction === '') {
       return data;
@@ -79,7 +60,6 @@ export class TbPersonaDataSource extends DataSource<TbPersonaItem>  {
   }
 }
 
-/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
 function compare(a: string | number, b: string | number, isAsc: boolean): number {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
--- src/app/tablas/tb-persona/tb-persona.component.ts
+++ src/app/tablas/tb-persona/tb-persona.component.ts
@@ -1,5 +1,5 @@
 import { PersonaService } from '../../services/persona.service';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
@@ -11,16 +11,18 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './tb-persona.component.html',
   styleUrls: ['./tb-persona.component.css']
 })
-export class TbPersonaComponent implements AfterViewInit {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<TbPersonaItem>;
+export class TbPersonaComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<TbPersonaItem>;
   dataSource: TbPersonaDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'nombre','apellido','fecha_nacimiento','direccion','acciones'];
 
   constructor(private personaService: PersonaService, private _snackBar: MatSnackBar) {
+  }
+  ngOnInit(): void {
     this.cargarUsuario();
   }
 
@@ -28,7 +30,6 @@ export class TbPersonaComponent implements AfterViewInit {
     this.dataSource = new TbPersonaDataSource();
     this.personaService.getPersona().subscribe(
       res=>{
-        console.log(res);
         this.dataSource.data = res;
       }
     )
